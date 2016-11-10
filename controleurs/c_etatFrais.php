eaf7af98025fcbb7b416a4f6dce4d88c51ca9000
zickler <zickler@J205-10.infogestion.local>
2016-11-10 12:34:39 +0100

    b

--- controleurs/c_etatFrais.php
+++ controleurs/c_etatFrais.php
@@ -12,10 +12,7 @@ switch ($action) {
 			// Afin de sélectionner par défaut le dernier mois dans la zone de liste
 			// on demande toutes les clés, et on prend la première,
 			// les mois étant triés décroissants
-			if (empty ( $lesMois )) {
-				$lesMois = $pdo->getMoisDisponibles ();
-				$lesCles = array_keys ( $lesMois );
-			}
+			$lesCles = array_keys( $lesMois );
 			$moisASelectionner = $lesCles [0];
 			include ("vues/v_listeMois.php");
 			break;
