2586be3b84c558286220ad2665ce86fe5b000ad7
zickler <zickler@J205-10.infogestion.local>
2016-11-10 11:47:37 +0100

    cumit

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -1,34 +1,44 @@
 <?php
-if(!isset($_REQUEST['action'])){
-	$_REQUEST['action'] = 'demandeConnexion';
+if (! isset ( $_REQUEST ['action'] )) {
+	$_REQUEST ['action'] = 'demandeConnexion';
 }
-$action = $_REQUEST['action'];
-switch($action){
-	case 'demandeConnexion':{
-		include("vues/v_connexion.php");
-		break;
-	}
-	case 'valideConnexion':{
-		$login = $_REQUEST['login'];
-		$mdp = $_REQUEST['mdp'];
-		$visiteur = $pdo->getInfosVisiteur($login,$mdp);
-		if(!is_array( $visiteur)){
-			ajouterErreur("Login ou mot de passe incorrect");
-			include("vues/v_erreurs.php");
-			include("vues/v_connexion.php");
+$action = $_REQUEST ['action'];
+switch ($action) {
+	case 'demandeConnexion' :
+		{
+			include ("vues/v_connexion.php");
+			break;
 		}
-		else{
-			$id = $visiteur['id'];
-			$nom =  $visiteur['nom'];
-			$prenom = $visiteur['prenom'];
-			connecter($id,$nom,$prenom);
-			include("vues/v_sommaire.php");
+	case 'valideConnexion' :
+		{
+			$login = $_REQUEST ['login'];
+			$mdp = $_REQUEST ['mdp'];
+			$visiteur = $pdo->getInfosVisiteur ( $login, $mdp );
+			$comptable = $pdo->getInfosComptable ( $login, $mdp );
+			if (! is_array ( $visiteur ) && ! is_array ( $comptable )) {
+				ajouterErreur ( "Login ou mot de passe incorrect" );
+				include ("vues/v_erreurs.php");
+				include ("vues/v_connexion.php");
+			} elseif (is_array ( $visiteur )) {
+				$id = $visiteur ['id'];
+				$nom = $visiteur ['nom'];
+				$prenom = $visiteur ['prenom'];
+				connecter ( $id, $nom, $prenom );
+				include ("vues/v_sommaire.php");
+			} elseif (is_array ( $comptable )) {
+				$id = $comptable ['id'];
+				$nom = $comptable ['login'];
+				$prenom = $comptable [2];
+				connecter ( $id, $nom, $prenom );	
+
+				include ("vues/v_sommaire.php");
+			}
+			break;
 		}
-		break;
-	}
-	default :{
-		include("vues/v_connexion.php");
-		break;
-	}
+	default :
+		{
+			include ("vues/v_connexion.php");
+			break;
+		}
 }
 ?>
