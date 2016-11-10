bbd4ac1bd80f05eac990c01cb68048475bfefc2a
zickler <zickler@J205-10.infogestion.local>
2016-11-10 11:28:07 +0100

    aaaaa

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -54,11 +54,14 @@ class PdoGsb{
  * @return l'id, le nom et le prénom sous la forme d'un tableau associatif 
 */
 	public function getInfosVisiteur($login, $mdp){
-		$req = "select visiteur.id as id, visiteur.nom as nom, visiteur.prenom as prenom from visiteur 
-		where visiteur.login='$login' and visiteur.mdp='$mdp'";
-		$res = PdoGsb::$monPdo->prepare($req);
-		$res->execute();
+		$res = PdoGsb::$monPdo->prepare("select visiteur.id as id, visiteur.nom as nom, visiteur.prenom as prenom from visiteur 
+		where visiteur.login=:login and visiteur.mdp=:mdp");
+		$res->execute(array(
+				'login'=>$login,
+				'mdp'=>$mdp
+		));
 		$ligne=$res->fetch();
+		var_dump($ligne);
 		return $ligne;
 	}
 
