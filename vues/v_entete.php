133e728a8c890e548417d2d52a1b34fc53460723
rodrigues <rodrigues@J205-09.infogestion.local>
2016-11-10 10:33:49 +0100

    fet

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -11,5 +11,5 @@
     <div id="page">
       <div id="entete">
         <img src="./images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
-        <h1>Suivi du remboursement des frais<br>LOL gros site</br></h1>
+        <h1>Suivi du remboursement des frais</h1>
       </div>
